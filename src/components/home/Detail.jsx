b0904edb50371241c57d0cbcc506f854b4307631
quangviet93 <quangviet.nguyen19@gmail.com>
2021-12-29 17:03:05 +0700

    mtpp

--- src/components/home/Detail.jsx
+++ src/components/home/Detail.jsx
@@ -9,22 +9,22 @@ import {
 
 const data = [
   {
-    img: "/imgs/mam-qua-cuoi-1-1024x791.jpg",
+    img: "mam-qua-cuoi-1-1024x791.jpg",
     title: "Chất Lượng Sản Phẩm Tốt Nhất :",
     des: "Mâm quả ngày cưới là một trong những nghi thức cưới hỏi mang đậm dấu ấn truyền thống của người Việt. Đặc biệt, trong các thủ tục đám cưới, đám hỏi của người Việt Nam, lễ gia tiên – ăn hỏi là một phần vô cùng quan trọng. Việc chuẩn bị lễ vật này thường được nhà trai ban bạc rồi đưa tới nhà gái trong ăn ăn hỏi, nên rất được chú trọng. Mâm quả này được xem như lời hứa hẹn của nhà trai trước khi xin rước cô gái về làm vợ, làm con dâu trong gia đình.",
   },
   {
-    img: "/imgs/mam-qua-cuoi-hoi.jpg",
+    img: "mam-qua-cuoi-hoi.jpg",
     title: "Chất Lượng Sản Phẩm Tốt Nhất :",
     des: "Mâm quả ngày cưới là một trong những nghi thức cưới hỏi mang đậm dấu ấn truyền thống của người Việt. Đặc biệt, trong các thủ tục đám cưới, đám hỏi của người Việt Nam, lễ gia tiên – ăn hỏi là một phần vô cùng quan trọng. Việc chuẩn bị lễ vật này thường được nhà trai ban bạc rồi đưa tới nhà gái trong ăn ăn hỏi, nên rất được chú trọng. Mâm quả này được xem như lời hứa hẹn của nhà trai trước khi xin rước cô gái về làm vợ, làm con dâu trong gia đình.",
   },
   {
-    img: "/imgs/mam-qua-ngay-cuoi-1.jpg",
+    img: "mam-qua-ngay-cuoi-1.jpg",
     title: "Chất Lượng Sản Phẩm Tốt Nhất :",
     des: "Mâm quả ngày cưới là một trong những nghi thức cưới hỏi mang đậm dấu ấn truyền thống của người Việt. Đặc biệt, trong các thủ tục đám cưới, đám hỏi của người Việt Nam, lễ gia tiên – ăn hỏi là một phần vô cùng quan trọng. Việc chuẩn bị lễ vật này thường được nhà trai ban bạc rồi đưa tới nhà gái trong ăn ăn hỏi, nên rất được chú trọng. Mâm quả này được xem như lời hứa hẹn của nhà trai trước khi xin rước cô gái về làm vợ, làm con dâu trong gia đình.",
   },
   {
-    img: "/imgs/hannah.jpg",
+    img: "hannah.jpg",
     title: "Chất Lượng Sản Phẩm Tốt Nhất :",
     des: "Mâm quả ngày cưới là một trong những nghi thức cưới hỏi mang đậm dấu ấn truyền thống của người Việt. Đặc biệt, trong các thủ tục đám cưới, đám hỏi của người Việt Nam, lễ gia tiên – ăn hỏi là một phần vô cùng quan trọng. Việc chuẩn bị lễ vật này thường được nhà trai ban bạc rồi đưa tới nhà gái trong ăn ăn hỏi, nên rất được chú trọng. Mâm quả này được xem như lời hứa hẹn của nhà trai trước khi xin rước cô gái về làm vợ, làm con dâu trong gia đình.",
   },
@@ -33,13 +33,14 @@ const shareUrl = "http://github.com";
 const title = "GitHub";
 
 const Footer = (props) => {
+  // static image
   return (
     <div className="detail-container">
       <div className="detail-title">
         {data.map((item, i) => (
           <div key={i} className="detail-item">
             <div className="detail-image">
-              <img src={item.img} />
+              <img src={`/imgs/${item.img}`} />
             </div>
             <div className="detail-content">
               <h3>{item.title}</h3>
